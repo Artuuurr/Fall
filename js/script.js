6776fb65d026d353e0d1bd5cf8c50dfc7defa9e6
Artur <shakirovartur60@gmail.com>
2023-07-27 04:41:49 +0500

    no code js

--- js/script.js
+++ js/script.js
@@ -8,12 +8,11 @@ let incr = 10,
 console.log(++incr);
 console.log(--decr);
 
-console.log(5%2);
 
 console.log(2*4 === 8);
 
 const isChecked = false;
       isClose = true;
 
-console.log(isChecked && isClose);
+console.log(isChecked || isClose);
 console.log();
